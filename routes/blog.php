09349af710aec5bd191a16662f5f033841b98710
Md. Mominul Islam <mominbd532@gmail.com>
2024-12-10 14:49:20 +0600

    rote update

--- routes/blog.php
+++ routes/blog.php
@@ -4,11 +4,10 @@ use App\Http\Controllers\BlogController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => 'auth:api'], function () {
-   Route::apiResource('blogs', BlogController::class)->middleware([
-      'index' => 'permission:view-blogs',
-      'show' => 'permission:view-blog',
-      'store' => 'permission:create-blog',
-      'update' => 'permission:edit-blog',
-      'destroy' => 'permission:delete-blog',
-   ]);
+   
+   Route::get('blogs', [BlogController::class, 'index'])->middleware('permission:view-blogs');
+   Route::get('blogs/{id}', [BlogController::class, 'show'])->middleware('permission:view-blog');
+   Route::post('blogs', [BlogController::class, 'store'])->middleware('permission:create-blog');
+   Route::put('blogs/{id}', [BlogController::class, 'update'])->middleware('permission:edit-blog');
+  
 });
